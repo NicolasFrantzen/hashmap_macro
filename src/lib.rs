507a7890cfb784d8282bcc8723d5b083263b8f4f
Nicolas Bru Frantzen <nicolas.bru.frantzen@gmail.com>
2022-07-22 19:35:36 +0200

    feat: create hashmap with expression arms

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,43 @@
-#![allow(unused_macros)]
-
-use std::collections::HashMap;
-
+#[macro_export]
 macro_rules! hashmap {
-    ($val:expr) => {
-        println!("Creating a hasmap macro with {}", $val);
-    };
+    () => (
+        std::collections::HashMap::new()
+    );
+    ($($key:expr => $val:expr$(,)?)+) => (
+        {
+            let mut hashmap = std::collections::HashMap::new();
+            $(hashmap.insert($key, $val);)*
+
+            hashmap
+        }
+    );
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    #[test]
-    fn test_hashmap_empty() {
-        //assert_eq!(hashmap!(), HashMap::new());
-    }
+    use std::collections::HashMap;
 
     #[test]
     fn test_hashmap() {
-        //hashmap!(1337);
+        let mut example = HashMap::new();
+        assert_eq!(hashmap!(), example);
+
+        example.insert("foo", 1337);
+        assert_eq!(hashmap!("foo" => 1337), example);
+        assert_eq!(hashmap!("foo" => 1337,), example);
+
+        example.insert("bar", 42);
+        assert_eq!(hashmap!("foo" => 1337, "bar" => 42), example);
+        assert_eq!(hashmap!("foo" => 1337, "bar" => 42,), example);
+    }
+
+    #[test]
+    fn test_hashmap_insert() {
+        let mut example = HashMap::new();
+        example.insert("bar", 69);
+
+        let mut hm = hashmap!();
+        hm.insert("bar", 69);
+        assert_eq!(example, hm);
     }
 }
