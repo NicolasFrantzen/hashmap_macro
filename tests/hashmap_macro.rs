7af6b5cb5e4374677868473003072c9c3ad8b68b
Nicolas Bru Frantzen <nicolas.bru.frantzen@gmail.com>
2022-07-23 12:39:45 +0200

    chore: moved unit tests to integration tests

---
+++ tests/hashmap_macro.rs
@@ -0,0 +1,43 @@
+use hashmap_macro::hashmap;
+use std::collections::HashMap;
+
+#[test]
+fn test_hashmap_tuple() {
+    let mut example = HashMap::new();
+    assert_eq!(hashmap!(), example);
+
+    example.insert("foo", 1337);
+    assert_eq!(hashmap!(("foo", 1337)), example);
+    assert_eq!(hashmap!(("foo", 1337),), example);
+
+    example.insert("bar", 42);
+    assert_eq!(hashmap!(("foo", 1337), ("bar", 42)), example);
+    assert_eq!(hashmap!(("foo", 1337), ("bar", 42),), example);
+}
+
+#[test]
+fn test_hashmap_arms() {
+    let mut example = HashMap::new();
+    assert_eq!(hashmap!(), example);
+
+    example.insert("foo", 1337);
+    assert_eq!(hashmap!("foo" => 1337), example);
+    assert_eq!(hashmap!("foo" => 1337,), example);
+
+    example.insert("bar", 42);
+    assert_eq!(hashmap!("foo" => 1337, "bar" => 42), example);
+    assert_eq!(hashmap!("foo" => 1337, "bar" => 42,), example);
+
+    example.insert("baz", 666);
+    assert_eq!(hashmap!({"foo" => 1337, "bar" => 42, "baz" => 666}), example);
+}
+
+#[test]
+fn test_hashmap_insert() {
+    let mut example = HashMap::new();
+    example.insert("bar", 69);
+
+    let mut hm = hashmap!();
+    hm.insert("bar", 69);
+    assert_eq!(example, hm);
+}
